13387d55f7f7c965061cf0bf661024d8a38625ab
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-03-13 20:16:49 +0530

    user logic added to the userController

---
+++ controllers/userController.js
@@ -0,0 +1,85 @@
+const {
+  genPassword,
+  comparePassword,
+  issueToken,
+} = require("../utils/passwordUtils");
+const User = require("../models/User");
+const CustomError = require("../middleware/CustomError");
+const asyncHandler = require("../utils/asyncHandler");
+
+const register = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const err = new CustomError("user already exists", 401);
+
+    return next(err);
+  }
+
+  const hashedPassword = await genPassword(password);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    image: `images/${req.file.filename}`,
+  });
+  await newUser.save();
+
+  const token = await issueToken(newUser);
+  console.log(token);
+  res.redirect("/");
+});
+
+const registerForm = asyncHandler(async (req, res, next) => {
+  res.render("register.ejs");
+});
+
+const loginForm = asyncHandler(async (req, res, next) => {
+  res.render("login.ejs");
+});
+
+const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  user;
+  if (!user) {
+    const err = new CustomError("User does not exists!", 404);
+    next(err);
+  }
+
+  const isValid = await comparePassword(password, user.password);
+
+  if (!isValid) {
+    const err = new CustomError("Enter a valid password", 401);
+    next(err);
+  }
+
+  res.redirect("/");
+});
+
+const getProfile = asyncHandler(async (req, res, next) => {
+  const users = await User.find({ _id: { $nin: [req.user._id] } });
+  res.render("dashboard.ejs", { user: req.user, users: users });
+});
+
+const logout = asyncHandler(async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/users/login");
+  });
+});
+
+module.exports = {
+  register,
+  registerForm,
+  loginForm,
+  login,
+  logout,
+  getProfile,
+};
