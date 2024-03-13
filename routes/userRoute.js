9318adebd29e6b5859b17947b999f2b2536425be
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-03-13 20:18:32 +0530

    user route added

---
+++ routes/userRoute.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  registerForm,
+  loginForm,
+  login,
+  logout,
+  getProfile,
+} = require("../controllers/userController");
+const upload = require("../config/multerConfig");
+const passport = require("passport");
+const { isAuth } = require("../middleware/isAuth.js");
+
+router
+  .get("/register", registerForm)
+  .get("/login", loginForm)
+  .get("/profile", isAuth, getProfile)
+  .get("/logout", isAuth, logout)
+  .post("/register", upload.single("image"), register)
+  .post(
+    "/login",
+    passport.authenticate("local", {
+      failureRedirect: "/users/login-failure",
+      successRedirect: "/users/profile",
+    }),
+    login
+  )
+  .get("/login-failure", (req, res, next) => {
+    res.send("You entered the wrong password.");
+  })
+  .get("/login-success", (req, res, next) => {
+    const html =
+      "<p>You have successfully logged in. <a href='/users/profile'>Go to protected Route</a></p>";
+
+    res.send(html);
+  });
+
+module.exports = router;
